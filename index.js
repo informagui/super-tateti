9311e2884727c349a64fb3469913ea913e61c930
Magali <magali.krueel@gmail.com>
2024-06-11 19:47:19 -0300

    HECHO EL SUPER TATETI

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+let turn = ""
 function jugar(cuadrado){
     var id = cuadrado.id;
     let queBotonEl = document.getElementById(id);
-    if ( queBotonEl.innerText == ""){
+    if ( turn == ""){
+        turn = "X"
         queBotonEl.innerText = "X"
-    } else if ( queBotonEl.innerText == "X"){
+    } else if ( turn == "X"){
+        turn = "O"
         queBotonEl.innerText = "O"
     } else {
+        turn = "X"
         queBotonEl.innerText = "X"
     }
     return id;
