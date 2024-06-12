5249b07c25d36428a6344f015be6f6b15e3c1fb9
Magali <magali.krueel@gmail.com>
2024-06-12 15:37:00 -0300

    see commit

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 let turn = ""
+let proximoCuadrante;
 function jugar(cuadrado){
-    var id = cuadrado.id;
+    var id = cuadranteActual(cuadrado);
     let queBotonEl = document.getElementById(id);
-    if ( turn == ""){
-        turn = "X"
-        queBotonEl.innerText = "X"
-    } else if ( turn == "X"){
-        turn = "O"
-        queBotonEl.innerText = "O"
-    } else {
-        turn = "X"
-        queBotonEl.innerText = "X"
+    if( queBotonEl.innerText == ""){
+        if ( turn == ""){
+            turn = "X"
+            queBotonEl.innerText = "X"
+        } else if ( turn == "X"){
+            turn = "O"
+            queBotonEl.innerText = "O"
+        } else {
+            turn = "X"
+            queBotonEl.innerText = "X"
+        }
+        var div  = document.getElementById(proximoCuadrante)
+        var table = div.getElementsByTagName('table')[0];
+        table.style.backgroundColor = 'lightblue'
+        return id;
     }
-    return id;
+}
+function cuadranteActual( cuadrante){
+    var id = cuadrante.id;
+    var charArray = id.split("");
+    proximoCuadrante = charArray[0] + charArray[1] + charArray[2];
+    return id 
+}
+function resetear(){
+    document.getElementById("seleccion").reset()
 }
