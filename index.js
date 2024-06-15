c561609cf6fd3e0c52248a2ef3ff923a7f4b7dcd
Magali <magali.krueel@gmail.com>
2024-06-15 18:59:40 -0300

    intento hacer input

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-let turn = ""
+let turn = "";
+let p1 = "X"; 
+let p2 = "O";
 let proximoCuadrante;
+function jugadores(){
+    let x = document.getElementById("seleccion")
+    p1 = x.elements[0].value
+    p2 = x.elements[1].value
+    let textoEl = document.getElementById("texto")
+    textoEl.innerText = p1 + p2
+}
 function jugar(cuadrado){
     var id = cuadranteActual(cuadrado);
     let queBotonEl = document.getElementById(id);
     if( queBotonEl.innerText == ""){
         if ( turn == ""){
-            turn = "X"
-            queBotonEl.innerText = "X"
-        } else if ( turn == "X"){
-            turn = "O"
-            queBotonEl.innerText = "O"
+            turn = p1
+            queBotonEl.innerText = p1
+        } else if ( turn == p1){
+            turn = p2
+            queBotonEl.innerText = p2
         } else {
-            turn = "X"
-            queBotonEl.innerText = "X"
+            turn = p1
+            queBotonEl.innerText = p1
         }
         document.getElementById(proximoCuadrante).style.backgroundColor = blue;
         return id;
